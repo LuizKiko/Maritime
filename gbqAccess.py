d1f8f797911c95cb917ea8cd64ba3d801fca0170
Luiz <150481961+LuizKiko@users.noreply.github.com>
2023-12-05 13:31:15 +0100

    Update gbqAccess.py

--- gbqAccess.py
+++ gbqAccess.py
@@ -3,6 +3,7 @@ import pandas as pd
 from relativeGeo import relativeGeo
 import os
 from google.cloud.bigquery.client import Client
+import streamlit as st
 import json
 from google.oauth2.service_account import Credentials
 
